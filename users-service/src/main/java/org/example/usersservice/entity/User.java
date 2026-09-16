86f13bbcb9da74fc0f9edba3cbc8696d857221e2
jefinagilbert <jefinagilbert>
2026-09-17 03:59:33 +0530

    first commit

---
+++ users-service/src/main/java/org/example/usersservice/entity/User.java
@@ -0,0 +1,48 @@
+package org.example.usersservice.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {}
+
+    public UUID getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+}
